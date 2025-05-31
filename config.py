ec70de9875ea8f24b20e78656672c457283066d6
harsh <155743690+Harshmanjhi870@users.noreply.github.com>
2025-05-31 22:46:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,17 +4,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot Configuration
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "YOUR_BOT_TOKEN_HERE")
-API_ID = int(os.getenv("API_ID", "YOUR_API_ID"))
-API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "7785667937:AAHKV2scpVH7SeyMGuyUZHaiYw-z4Rnpj9c")
+API_ID = int(os.getenv("API_ID", "23255238"))
+API_HASH = os.getenv("API_HASH", "009e3d8c1bdc89d5387cdd8fd182ec15")
 
-# Database Configuration
-MONGO_URI = os.getenv("MONGO_URI", "YOUR_MONGO_URI")
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://nibbanmisal3302:Gokukhan3303@cluster0.0u22b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.getenv("DB_NAME", "antakshari_bot_db")
 
+
 # Bot Settings
-OWNER_ID = int(os.getenv("OWNER_ID", "YOUR_OWNER_ID"))
-BOT_NAME = os.getenv("BOT_NAME", "Antakshari Game Bot")
+OWNER_ID = int(os.getenv("OWNER_ID", "7073835511"))
+BOT_NAME = os.getenv("BOT_NAME", "Atlas")
 LOGGER = True
 
 # Game Settings
